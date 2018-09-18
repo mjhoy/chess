4489ab886b20b0e90f62f3fad08b818ef40288d8
Michael Hoy <mjh@mjhoy.com>
2018-09-18 07:12:54 -0400

    split to lib/bin targets

---
+++ src/bin/main.rs
@@ -0,0 +1,53 @@
+use std::io;
+
+extern crate chess;
+use chess::{new_game, board_str, gen_moves, player_str, move_str};
+
+fn main() {
+    let mut game = new_game();
+    let ended: &mut bool = &mut false;
+    let mut buf = String::new();
+
+    while !*ended {
+        println!("\n{}", board_str(&game));
+
+        let moves = gen_moves(game.state);
+
+        if moves.len() == 0 {
+            println!("Game over! RET quits.");
+            io::stdin().read_line(&mut buf).unwrap();
+            *ended = true;
+            break;
+        }
+
+        println!("{}'s move.", player_str(game.state.current_player));
+        for i in 0..moves.len() {
+            let m0ve = &moves[i];
+            println!("{}: {}", i+1, move_str(&m0ve));
+        }
+
+        println!("Enter {}..{} ('q' quits)", 1, moves.len());
+
+        io::stdin().read_line(&mut buf).unwrap();
+
+        if buf.trim() == "q" {
+            *ended = true;
+            break;
+        }
+
+        match buf.trim().parse::<usize>() {
+            Ok(i) => {
+                if i <= moves.len() && i > 0 {
+                    game = moves.into_iter().nth(i - 1).expect("checked index").next;
+                } else {
+                    println!("Please enter a number between {} and {}.", 1, moves.len());
+                }
+            }
+            Err(_) => {
+                println!("Please enter a number between {} and {}.", 1, moves.len());
+            }
+        }
+
+        buf = String::new();
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
-use std::io;
-
 extern crate nalgebra as na;
 #[macro_use] extern crate itertools;
 
 use na::{U2, U3, MatrixArray, Matrix, RowVector2};
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-enum Player {
+pub enum Player {
     White,
     Black,
 }
@@ -14,7 +12,7 @@ enum Player {
 use Player::*;
 
 impl Player {
-    fn other(&self) -> Player {
+    pub fn other(&self) -> Player {
         match &self {
             White => Black,
             Black => White,
@@ -23,36 +21,36 @@ impl Player {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-enum Piece {
+pub enum Piece {
     Pawn,
 }
 
 use Piece::*;
 
-type Square = Option<(Player, Piece)>;
+pub type Square = Option<(Player, Piece)>;
 
 #[derive(Debug, Clone, Copy)]
-struct Pos {
+pub struct Pos {
     rank: u8,
     file: u8
 }
 
-// start with a 2x3 board
-type Board = Matrix<Square, U3, U2, MatrixArray<Square, U3, U2>>;
-
 #[derive(Debug, Clone, Copy)]
-struct GameState {
-    board: Board,
-    current_player: Player,
+pub struct GameState {
+    pub board: Board,
+    pub current_player: Player,
 }
 
-struct Game {
-    state: GameState,
+// start with a 2x3 board
+pub type Board = Matrix<Square, U3, U2, MatrixArray<Square, U3, U2>>;
+
+pub struct Game {
+    pub state: GameState,
 }
 
-struct Move {
+pub struct Move {
     index: (Pos,Pos),
-    next: Game
+    pub next: Game
 }
 
 fn initial_board() -> Board {
@@ -73,7 +71,7 @@ fn piece_at(pos: &Pos, board: &Board) -> Option<(Player,Piece)> {
     board.row(pos.rank as usize)[pos.file as usize]
 }
 
-fn new_game() -> Game {
+pub fn new_game() -> Game {
     let board = initial_board();
     let current_player = White;
     let state = GameState { board, current_player, };
@@ -83,7 +81,7 @@ fn new_game() -> Game {
 }
 
 // movement logic
-fn can_move(board: &Board, player: &Player, from_pos: &Pos, to_pos: &Pos) -> bool {
+pub fn can_move(board: &Board, player: &Player, from_pos: &Pos, to_pos: &Pos) -> bool {
     fn can_move_pawn(player: &Player, from_pos: &Pos, to_pos: &Pos, capture: bool) -> bool {
         let next_rank = from_pos.rank as i32 + if *player == White { 1 } else { -1 };
         if to_pos.rank != next_rank as u8 { return false; }
@@ -114,7 +112,7 @@ fn can_move(board: &Board, player: &Player, from_pos: &Pos, to_pos: &Pos) -> boo
     }
 }
 
-fn move_piece(board: &Board, from_pos: &Pos, to_pos: &Pos) -> Board {
+pub fn move_piece(board: &Board, from_pos: &Pos, to_pos: &Pos) -> Board {
     let new_board: &mut Board = &mut board.clone();
     let from = piece_at(from_pos, board);
     new_board[(from_pos.rank as usize, from_pos.file as usize)] = None;
@@ -124,7 +122,7 @@ fn move_piece(board: &Board, from_pos: &Pos, to_pos: &Pos) -> Board {
 }
 
 // On^2 for n squares
-fn gen_moves(state: GameState) -> Vec<Move> {
+pub fn gen_moves(state: GameState) -> Vec<Move> {
     coords(&state.board).iter().flat_map(|from_pos| {
         coords(&state.board).iter().filter_map(|to_pos| {
             if can_move(&state.board, &state.current_player, from_pos, to_pos) {
@@ -144,7 +142,7 @@ fn gen_moves(state: GameState) -> Vec<Move> {
     }).collect()
 }
 
-fn board_str(game: &Game) -> String {
+pub fn board_str(game: &Game) -> String {
     fn piece_str(square: &Square) -> String {
         let piece_str = match square {
             None => " ",
@@ -167,7 +165,7 @@ fn board_str(game: &Game) -> String {
     buf
 }
 
-fn move_str(m0ve: &Move) -> String {
+pub fn move_str(m0ve: &Move) -> String {
     let (from, to) = m0ve.index;
     let from_file = (from.file + 'A' as u8) as char;
     let from_rank = from.rank + 1;
@@ -176,58 +174,9 @@ fn move_str(m0ve: &Move) -> String {
     format!("{}{} -> {}{}", from_file, from_rank, to_file, to_rank)
 }
 
-fn player_str(player: Player) -> &'static str {
+pub fn player_str(player: Player) -> &'static str {
     match player {
         White => "White",
         Black => "Black",
-    }
-}
-
-fn main() {
-    let mut game = new_game();
-    let ended: &mut bool = &mut false;
-    let mut buf = String::new();
-
-    while !*ended {
-        println!("\n{}", board_str(&game));
-
-        let moves = gen_moves(game.state);
-
-        if moves.len() == 0 {
-            println!("Game over! RET quits.");
-            io::stdin().read_line(&mut buf).unwrap();
-            *ended = true;
-            break;
-        }
-
-        println!("{}'s move.", player_str(game.state.current_player));
-        for i in 0..moves.len() {
-            let m0ve = &moves[i];
-            println!("{}: {}", i+1, move_str(&m0ve));
-        }
-
-        println!("Enter {}..{} ('q' quits)", 1, moves.len());
-
-        io::stdin().read_line(&mut buf).unwrap();
-
-        if buf.trim() == "q" {
-            *ended = true;
-            break;
-        }
-
-        match buf.trim().parse::<usize>() {
-            Ok(i) => {
-                if i <= moves.len() && i > 0 {
-                    game = moves.into_iter().nth(i - 1).expect("checked index").next;
-                } else {
-                    println!("Please enter a number between {} and {}.", 1, moves.len());
-                }
-            }
-            Err(_) => {
-                println!("Please enter a number between {} and {}.", 1, moves.len());
-            }
-        }
-
-        buf = String::new();
     }
 }
