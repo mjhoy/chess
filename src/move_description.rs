728e2d2642f65023e77b4fc76c691712b016ba17
Michael Hoy <mjh@mjhoy.com>
2019-03-31 09:04:33 -0400

    MoveDescription::match_moves

--- src/move_description.rs
+++ src/move_description.rs
@@ -1,3 +1,4 @@
+use crate::m0ve::Move;
 use crate::piece::Piece;
 use crate::pos::Pos;
 use std::result::Result;
@@ -17,14 +18,36 @@ impl MoveDescription {
             Err(e) => Err(format!("parsing error: {:?}", e)),
         }
     }
+
+    pub fn match_moves(&self, moves: Vec<Move>) -> Option<Move> {
+        for m0ve in moves {
+            if self.match_move(&m0ve) {
+                return Some(m0ve.clone());
+            }
+        }
+        None
+    }
+
+    fn match_move(&self, m0ve: &Move) -> bool {
+        let (_from, to) = m0ve.index;
+        let dst_piece = m0ve
+            .next
+            .state
+            .board
+            .piece_at(to)
+            .map(|(_player, piece)| piece);
+        self.dst_pos == to && Some(self.src_piece) == dst_piece
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::game::Game;
+    use crate::player::Player;
 
     #[test]
-    fn test_parse_move_description() {
+    fn test_parse() {
         assert_eq!(
             MoveDescription::parse("Ke2"),
             Ok(MoveDescription {
@@ -35,6 +58,32 @@ mod test {
         assert_eq!(
             MoveDescription::parse("Ze2"),
             Err(r#"parsing error: Error(Code("Ze2", Alt))"#.to_string())
+        );
+    }
+
+    fn test_game() -> Game {
+        Game::initial()
+    }
+
+    #[test]
+    fn test_match_moves() {
+        let mut game = test_game();
+        for desc in vec!["e3", "e6", "Ke2", "e5", "Kd3", "e4"] {
+            let next_moves = game.state.gen_moves();
+            let move_desc = MoveDescription::parse(desc).unwrap();
+            game = move_desc.match_moves(next_moves).unwrap().next;
+        }
+
+        let d3 = MoveDescription::parse("d3").unwrap();
+        assert_eq!(
+            game.state.board.piece_at(d3.dst_pos),
+            Some((Player::White, Piece::King))
+        );
+
+        let e4 = MoveDescription::parse("e4").unwrap();
+        assert_eq!(
+            game.state.board.piece_at(e4.dst_pos),
+            Some((Player::Black, Piece::Pawn))
         );
     }
 }
