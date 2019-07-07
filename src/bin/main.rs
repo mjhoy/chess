b9990c1dc26a52a3e204dcee4e0a35681b16a83a
Michael Hoy <mjh@mjhoy.com>
2019-07-07 08:32:48 -0400

    add parsing of FEN to cli

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,48 @@
 use std::io;
 
+use chess::fen::fen;
+use chess::game::Game;
 use chess::move_description::MoveDescription;
 use chess::new_game;
+use clap::{App, Arg, SubCommand};
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 fn main() {
-    let mut game = new_game();
+    let matches = App::new("chess")
+        .version(VERSION)
+        .version_short("v")
+        .about("do some chess stuff on the command line")
+        .arg(
+            Arg::with_name("fen")
+                .short("f")
+                .long("fen")
+                .value_name("FEN")
+                .help("Position in Forsyth-Edwards notation"),
+        )
+        .subcommand(SubCommand::with_name("play").about("play a game"))
+        .get_matches();
+
+    let mut game;
+    if let Some(fen_str) = matches.value_of("fen") {
+        let result = fen(fen_str);
+        if let Ok((_, state)) = result {
+            game = Game::with_state(state);
+        } else {
+            panic!("Couldn't parse fen: {:?}", result);
+        }
+    } else {
+        game = new_game();
+    }
+
+    if let Some(_) = matches.subcommand_matches("play") {
+        play(game);
+    } else {
+        println!("{}", game.state.board.str());
+    }
+}
+
+fn play(mut game: Game) {
     let ended: &mut bool = &mut false;
     let mut buf = String::new();
 
