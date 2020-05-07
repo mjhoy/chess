b2d4199c3c752b3c76432bea1dfecf669f881982
Michael Hoy <mjh@mjhoy.com>
2020-05-07 08:54:34 -0400

    castling state logic

--- src/state.rs
+++ src/state.rs
@@ -1,6 +1,7 @@
 use crate::{
-    board::Board, castling::Castling, from_to_step::FromToStep, m0ve::Action, m0ve::Move,
-    piece::Piece, piece::Piece::*, player::Player, player::Player::*, pos::Pos,
+    board::Board, castling::CastleAbility, castling::Castling, from_to_step::FromToStep,
+    m0ve::Action, m0ve::Move, piece::Piece, piece::Piece::*, player::Player, player::Player::*,
+    pos, pos::Pos,
 };
 use itertools::Itertools;
 
@@ -20,7 +21,7 @@ impl State {
             board: self.board.clone(),
             player: self.player.other(),
             en_passant: None,
-            castling: self.castling.clone(),
+            castling: self.castling,
         };
 
         for from_pos in self.board.coords() {
@@ -58,19 +59,23 @@ impl State {
         }
     }
 
+    pub fn move_puts_current_player_in_check(&self, from_pos: Pos, to_pos: Pos) -> bool {
+        let next_state = State {
+            player: self.player,
+            board: self.board.move_piece(from_pos, to_pos),
+            en_passant: None,
+            castling: self.castling,
+        };
+        next_state.in_check()
+    }
+
     /// Can the current player move the piece in `from_pos` to `to_pos`?
     pub fn can_move(&self, from_pos: Pos, to_pos: Pos) -> bool {
         if !self.can_move_pseudo(from_pos, to_pos) {
             return false;
         }
 
-        let next_state = State {
-            player: self.player,
-            board: self.board.move_piece(from_pos, to_pos),
-            en_passant: None,
-            castling: Castling::initial(),
-        };
-        !next_state.in_check()
+        !self.move_puts_current_player_in_check(from_pos, to_pos)
     }
 
     fn en_passant_pos(&self, from: Pos, to: Pos) -> Option<Pos> {
@@ -106,14 +111,165 @@ impl State {
         } else {
             self.board.move_piece(from, to)
         };
+        let next_castling = match self.player {
+            Player::White => match from {
+                pos::e1 => Castling {
+                    white: CastleAbility {
+                        king: false,
+                        queen: false,
+                    },
+                    black: self.castling.black,
+                },
+                pos::h1 => Castling {
+                    white: CastleAbility {
+                        king: false,
+                        queen: self.castling.white.queen,
+                    },
+                    black: self.castling.black,
+                },
+                pos::a1 => Castling {
+                    white: CastleAbility {
+                        king: self.castling.white.king,
+                        queen: false,
+                    },
+                    black: self.castling.black,
+                },
+                _ => self.castling,
+            },
+            Player::Black => match from {
+                pos::e8 => Castling {
+                    black: CastleAbility {
+                        king: false,
+                        queen: false,
+                    },
+                    white: self.castling.white,
+                },
+                pos::h8 => Castling {
+                    black: CastleAbility {
+                        king: false,
+                        queen: self.castling.black.queen,
+                    },
+                    white: self.castling.black,
+                },
+                pos::a8 => Castling {
+                    black: CastleAbility {
+                        king: self.castling.black.king,
+                        queen: false,
+                    },
+                    white: self.castling.black,
+                },
+                _ => self.castling,
+            },
+        };
         let next_state = State {
             board: next_board,
             player: self.player.other(),
             en_passant: self.en_passant_pos(from, to),
-            castling: Castling::initial(),
+            castling: next_castling,
         };
         Move {
             action: Action::Simple { from, to },
+            next: next_state,
+        }
+    }
+
+    // Can the current player castle, not taking into account whether
+    // they have moved the king or rook already?
+    fn can_castle_psuedo(&self, kingside: bool) -> bool {
+        match (self.player, kingside) {
+            (Player::White, true) => {
+                self.board.piece_at(pos::e1) == Some((self.player, Piece::King))
+                    && self.board.piece_at(pos::h1) == Some((self.player, Piece::Rook))
+                    && self.board.empty_at(pos::f1)
+                    && self.board.empty_at(pos::g1)
+                    && !self.move_puts_current_player_in_check(pos::e1, pos::f1)
+                    && !self.move_puts_current_player_in_check(pos::e1, pos::g1)
+            }
+            (Player::White, false) => {
+                self.board.piece_at(pos::e1) == Some((self.player, Piece::King))
+                    && self.board.piece_at(pos::a1) == Some((self.player, Piece::Rook))
+                    && self.board.empty_at(pos::b1)
+                    && self.board.empty_at(pos::c1)
+                    && self.board.empty_at(pos::d1)
+                    && !self.move_puts_current_player_in_check(pos::e1, pos::d1)
+                    && !self.move_puts_current_player_in_check(pos::e1, pos::c1)
+            }
+            (Player::Black, true) => {
+                self.board.piece_at(pos::e8) == Some((self.player, Piece::King))
+                    && self.board.piece_at(pos::h8) == Some((self.player, Piece::Rook))
+                    && self.board.empty_at(pos::f8)
+                    && self.board.empty_at(pos::g8)
+                    && !self.move_puts_current_player_in_check(pos::e8, pos::f8)
+                    && !self.move_puts_current_player_in_check(pos::e8, pos::g8)
+            }
+            (Player::Black, false) => {
+                self.board.piece_at(pos::e8) == Some((self.player, Piece::King))
+                    && self.board.piece_at(pos::a8) == Some((self.player, Piece::Rook))
+                    && self.board.empty_at(pos::b8)
+                    && self.board.empty_at(pos::c8)
+                    && self.board.empty_at(pos::d8)
+                    && !self.move_puts_current_player_in_check(pos::e8, pos::d8)
+                    && !self.move_puts_current_player_in_check(pos::e8, pos::c8)
+            }
+        }
+    }
+
+    fn can_castle(&self, kingside: bool) -> bool {
+        let allowed = match (self.player, kingside) {
+            (Player::White, true) => self.castling.white.king,
+            (Player::White, false) => self.castling.white.queen,
+            (Player::Black, true) => self.castling.black.king,
+            (Player::Black, false) => self.castling.black.queen,
+        };
+        allowed && !self.in_check() && self.can_castle_psuedo(kingside)
+    }
+
+    fn build_castle_move(&self, kingside: bool) -> Move {
+        let (next_board, next_castling) = if self.player.is_white() {
+            let next_castling = Castling {
+                white: CastleAbility {
+                    king: false,
+                    queen: false,
+                },
+                black: self.castling.black,
+            };
+            let next_board = if kingside {
+                self.board
+                    .move_piece(pos::e1, pos::g1)
+                    .move_piece(pos::h1, pos::f1)
+            } else {
+                self.board
+                    .move_piece(pos::e1, pos::c1)
+                    .move_piece(pos::a1, pos::d1)
+            };
+            (next_board, next_castling)
+        } else {
+            let next_castling = Castling {
+                black: CastleAbility {
+                    king: false,
+                    queen: false,
+                },
+                white: self.castling.white,
+            };
+            let next_board = if kingside {
+                self.board
+                    .move_piece(pos::e8, pos::g8)
+                    .move_piece(pos::h8, pos::f8)
+            } else {
+                self.board
+                    .move_piece(pos::e8, pos::c8)
+                    .move_piece(pos::a8, pos::d8)
+            };
+            (next_board, next_castling)
+        };
+        let next_state = State {
+            board: next_board,
+            player: self.player.other(),
+            en_passant: self.en_passant,
+            castling: next_castling,
+        };
+        Move {
+            action: Action::Castle { kingside },
             next: next_state,
         }
     }
@@ -122,7 +278,14 @@ impl State {
     /// On^2 for n squares
     pub fn gen_moves(&self) -> Vec<Move> {
         let coords = self.board.coords();
-        coords
+        let castles = [true, false].iter().filter_map(|&kingside| {
+            if self.can_castle(kingside) {
+                Some(self.build_castle_move(kingside))
+            } else {
+                None
+            }
+        });
+        let simples = coords
             .iter()
             .cartesian_product(coords.iter())
             .filter_map(|(&from, &to)| {
@@ -131,8 +294,8 @@ impl State {
                 } else {
                     None
                 }
-            })
-            .collect()
+            });
+        castles.chain(simples).collect()
     }
 }
 
@@ -444,5 +607,243 @@ mod test {
         let (_, initial_state) =
             fen("rnbqkbnr/ppppp1p1/7p/4Pp2/8/8/PPPP1PPP/RNBQKBNR w KQkq -").unwrap();
         assert!(!initial_state.can_move(e5, f6));
+    }
+
+    #[test]
+    fn test_castling_white_kingside_allowed() {
+        let (_, initial_state) =
+            fen("rnbqkb1r/pp2pppp/3p1n2/2p5/2B5/4PN2/PPPP1PPP/RNBQK2R w KQkq - 0 4").unwrap();
+        assert!(initial_state.can_castle(true));
+        let next_state = initial_state.build_castle_move(true).next;
+        assert_eq!(
+            next_state.board.piece_at(g1),
+            Some((Player::White, Piece::King))
+        );
+        assert_eq!(
+            next_state.board.piece_at(f1),
+            Some((Player::White, Piece::Rook))
+        );
+        assert_eq!(next_state.castling.white.king, false);
+        assert_eq!(next_state.castling.white.queen, false);
+        assert_eq!(next_state.castling.black.king, true);
+        assert_eq!(next_state.castling.black.queen, true);
+    }
+
+    #[test]
+    fn test_castling_white_kingside_not_allowed() {
+        let (_, initial_state) =
+            fen("rnbqk2r/pp2ppbp/3p1np1/2p5/2B5/4PN2/PPPP1PPP/RNBQK2R w Qkq - 2 6").unwrap();
+        assert!(!initial_state.can_castle(true));
+    }
+
+    #[test]
+    fn test_castling_white_kingside_not_allowed_after_rook_move() {
+        let (_, initial_state) =
+            fen("rnbqkb1r/pp2pppp/3p1n2/2p5/2B5/4PN2/PPPP1PPP/RNBQK2R w KQkq - 0 4").unwrap();
+        assert!(initial_state.can_castle(true));
+        let next_state = initial_state.build_simple_move(h1, g1).next;
+        assert_eq!(next_state.castling.white.king, false);
+        assert_eq!(next_state.castling.white.queen, true);
+    }
+
+    #[test]
+    fn test_castling_white_queenside_allowed() {
+        let (_, initial_state) =
+            fen("rnbqkb1r/pp3ppp/2p1pn2/3p4/3P1B2/2NQ4/PPP1PPPP/R3KBNR w KQkq - 0 5").unwrap();
+        assert!(initial_state.can_castle(false));
+        let next_state = initial_state.build_castle_move(false).next;
+        assert_eq!(
+            next_state.board.piece_at(c1),
+            Some((Player::White, Piece::King))
+        );
+        assert_eq!(
+            next_state.board.piece_at(d1),
+            Some((Player::White, Piece::Rook))
+        );
+        assert_eq!(next_state.castling.white.king, false);
+        assert_eq!(next_state.castling.white.queen, false);
+        assert_eq!(next_state.castling.black.king, true);
+        assert_eq!(next_state.castling.black.queen, true);
+    }
+
+    #[test]
+    fn test_castling_white_queenside_not_allowed() {
+        let (_, initial_state) =
+            fen("rnbqkb1r/pp3ppp/2p1pn2/3p4/3P1B2/2NQ4/PPP1PPPP/1R2KBNR b Kkq - 1 5").unwrap();
+        assert!(!initial_state.can_castle(false));
+    }
+
+    #[test]
+    fn test_castling_white_not_allowed_after_rook_move() {
+        let (_, initial_state) =
+            fen("rnbqkb1r/pp3ppp/2p1pn2/3p4/3P1B2/2NQ4/PPP1PPPP/R3KBNR w KQkq - 0 5").unwrap();
+        assert!(initial_state.can_castle(false));
+        let next_state = initial_state.build_simple_move(a1, b1).next;
+        assert_eq!(next_state.castling.white.king, true);
+        assert_eq!(next_state.castling.white.queen, false);
+    }
+
+    #[test]
+    fn test_castling_white_queenside_not_allowed_after_king_move() {
+        let (_, initial_state) =
+            fen("rnbqkb1r/pp3ppp/2p1pn2/3p4/3P1B2/2NQ4/PPP1PPPP/R3KBNR w KQkq - 0 5").unwrap();
+        assert!(initial_state.can_castle(false));
+        let next_state = initial_state.build_simple_move(e1, f1).next;
+        assert_eq!(next_state.castling.white.king, false);
+        assert_eq!(next_state.castling.white.queen, false);
+    }
+
+    #[test]
+    fn test_castling_white_kingside_not_allowed_out_of_check_move() {
+        let (_, initial_state) =
+            fen("rnbqk1nr/pp1p2pp/2p1pp2/8/1b6/3PPN2/PPP1BPPP/RNBQK2R w KQkq - 2 5").unwrap();
+        assert!(!initial_state.can_castle(true));
+    }
+
+    #[test]
+    fn test_castling_white_kingside_not_allowed_through_check_on_f1_move() {
+        let (_, initial_state) =
+            fen("rn1qkbnr/ppp1pppp/B2p4/8/2b5/4PN2/PPPP1PPP/RNBQK2R w KQkq - 4 4").unwrap();
+        assert!(!initial_state.can_castle(true));
+    }
+
+    #[test]
+    fn test_castling_white_kingside_not_allowed_through_check_on_g1_move() {
+        let (_, initial_state) =
+            fen("rnbqk2r/pp1p1ppp/2p1p2n/2b5/4PP2/3B1N2/PPPP2PP/RNBQK2R w KQkq - 2 5").unwrap();
+        assert!(!initial_state.can_castle(true));
+    }
+
+    #[test]
+    fn test_castling_white_queenside_not_allowed_through_check_on_d1_move() {
+        let (_, initial_state) =
+            fen("rn1qk2r/ppp1bppp/3p1n2/4p1B1/3PP1b1/2NQ4/PPP2PPP/R3KBNR w KQkq - 4 6").unwrap();
+        assert!(!initial_state.can_castle(false));
+    }
+
+    #[test]
+    fn test_castling_white_queenside_not_allowed_through_check_on_c1_move() {
+        let (_, initial_state) =
+            fen("rnbqk2r/ppp1pp1p/3p1npb/8/3P4/2NQ4/PPP1PPPP/R3KBNR w KQkq - 2 5").unwrap();
+        assert!(!initial_state.can_castle(false));
+    }
+
+    #[test]
+    fn test_castling_black_kingside_allowed() {
+        let (_, initial_state) =
+            fen("rnbqk2r/pppp1ppp/5n2/2b1p3/4P3/3P4/PPPB1PPP/RN1QKBNR b KQkq - 2 4").unwrap();
+        assert!(initial_state.can_castle(true));
+        let next_state = initial_state.build_castle_move(true).next;
+        assert_eq!(
+            next_state.board.piece_at(g8),
+            Some((Player::Black, Piece::King))
+        );
+        assert_eq!(
+            next_state.board.piece_at(f8),
+            Some((Player::Black, Piece::Rook))
+        );
+        assert_eq!(next_state.castling.black.king, false);
+        assert_eq!(next_state.castling.black.queen, false);
+        assert_eq!(next_state.castling.white.king, true);
+        assert_eq!(next_state.castling.white.queen, true);
+    }
+
+    #[test]
+    fn test_castling_black_kingside_not_allowed() {
+        let (_, initial_state) =
+            fen("rnbqk1r1/pppp1ppp/5n2/2b1p3/4P3/3P4/PPPB1PPP/RN1QKBNR w KQq - 3 5").unwrap();
+        assert!(!initial_state.can_castle(true));
+    }
+
+    #[test]
+    fn test_castling_black_kingside_not_allowed_after_rook_move() {
+        let (_, initial_state) =
+            fen("rnbqk2r/pppp1ppp/5n2/2b1p3/4P3/3P4/PPPB1PPP/RN1QKBNR b KQkq - 2 4").unwrap();
+        assert!(initial_state.can_castle(true));
+        let next_state = initial_state.build_simple_move(h8, g8).next;
+        assert_eq!(next_state.castling.black.king, false);
+        assert_eq!(next_state.castling.black.queen, true);
+    }
+
+    #[test]
+    fn test_castling_black_not_allowed_after_king_move() {
+        let (_, initial_state) =
+            fen("rnbqk2r/pppp1ppp/5n2/2b1p3/4P3/3P4/PPPB1PPP/RN1QKBNR b KQkq - 2 4").unwrap();
+        assert!(initial_state.can_castle(true));
+        let next_state = initial_state.build_simple_move(e8, f8).next;
+        assert_eq!(next_state.castling.black.king, false);
+        assert_eq!(next_state.castling.black.queen, false);
+    }
+
+    #[test]
+    fn test_castling_black_queenside_allowed() {
+        let (_, initial_state) =
+            fen("r3kbnr/pppqpppp/2npb3/8/3P4/2P1PN2/PP3PPP/RNBQKB1R b KQkq - 0 5").unwrap();
+        assert!(initial_state.can_castle(false));
+        let next_state = initial_state.build_castle_move(false).next;
+        assert_eq!(
+            next_state.board.piece_at(c8),
+            Some((Player::Black, Piece::King))
+        );
+        assert_eq!(
+            next_state.board.piece_at(d8),
+            Some((Player::Black, Piece::Rook))
+        );
+        assert_eq!(next_state.castling.black.king, false);
+        assert_eq!(next_state.castling.black.queen, false);
+        assert_eq!(next_state.castling.white.king, true);
+        assert_eq!(next_state.castling.white.queen, true);
+    }
+
+    #[test]
+    fn test_castling_black_queenside_not_allowed() {
+        let (_, initial_state) =
+            fen("1r2kbnr/pppqpppp/2npb3/8/3P4/2P1PN2/PP3PPP/RNBQKB1R w KQk - 1 6").unwrap();
+        assert!(!initial_state.can_castle(false));
+    }
+
+    #[test]
+    fn test_castling_black_queenside_not_allowed_after_rook_move() {
+        let (_, initial_state) =
+            fen("r3kbnr/pppqpppp/2npb3/8/3P4/2P1PN2/PP3PPP/RNBQKB1R b KQkq - 0 5").unwrap();
+        assert!(initial_state.can_castle(false));
+        let next_state = initial_state.build_simple_move(a8, b8).next;
+        assert_eq!(next_state.castling.black.king, true);
+        assert_eq!(next_state.castling.black.queen, false);
+    }
+
+    #[test]
+    fn test_castling_black_kingside_not_allowed_out_of_check_move() {
+        let (_, initial_state) =
+            fen("rnbqk2r/ppp1pp1p/5npb/1B1p4/8/2N1PN1P/PPPP1PP1/R1BQK2R b KQkq - 2 5").unwrap();
+        assert!(!initial_state.can_castle(true));
+    }
+
+    #[test]
+    fn test_castling_black_kingside_not_allowed_through_check_on_f8_move() {
+        let (_, initial_state) =
+            fen("rnbqk2r/pppp1ppp/5n2/4p3/8/BP1P1N2/P1P1PPPP/RN1QKB1R b KQkq - 2 4").unwrap();
+        assert!(!initial_state.can_castle(true));
+    }
+
+    #[test]
+    fn test_castling_black_kingside_not_allowed_through_check_on_g8_move() {
+        let (_, initial_state) =
+            fen("rnbqk2r/pppp2pp/5p1n/2b1p3/2B5/2NPPN2/PPP2PPP/R1BQK2R b KQkq - 0 5").unwrap();
+        assert!(!initial_state.can_castle(true));
+    }
+
+    #[test]
+    fn test_castling_black_queenside_not_allowed_through_check_on_d8_move() {
+        let (_, initial_state) =
+            fen("r3kbnr/pp1qpppp/n1p5/B2p1b2/8/3PPN1P/PPP2PP1/RN1QKB1R b KQkq - 2 6").unwrap();
+        assert!(!initial_state.can_castle(false));
+    }
+
+    #[test]
+    fn test_castling_black_queenside_not_allowed_through_check_on_c8_move() {
+        let (_, initial_state) =
+            fen("r3kbnr/p1pqpppp/Bpnp4/8/3P4/2P1PNP1/PP3P1P/RNBQK2R b KQkq - 0 6").unwrap();
+        assert!(!initial_state.can_castle(false));
     }
 }
